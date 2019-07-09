036ae72a0d5f52ce170767d05a635466b1e81aff
Israel Pestilho <piparopa@gmail.com>
2019-07-09 11:20:58 -0300

    alteracoes

--- src/android/SecureKeyStore.java
+++ src/android/SecureKeyStore.java
@@ -32,7 +32,6 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.security.auth.x500.X500Principal;
-import javax.xml.bind.DatatypeConverter;
 
 public class SecureKeyStore extends CordovaPlugin {
 
